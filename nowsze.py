59b11e71b0558dc69ff1c0a7644a4d5b1ee491b9
unknown <mlisowski8.01@gmail.com>
2023-04-13 01:37:18 +0200

    ulepszony

--- nowsze.py
+++ nowsze.py
@@ -1,11 +1,10 @@
+import os
 import discord
 from discord.ext import commands
 import random
 import time
 
-with open("tokenisko.py", "r") as f:
-    TOKEN = f.read().strip()
-
+from config import TOKEN
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
@@ -13,6 +12,9 @@ bot.tank_reactions = []
 bot.healer_reactions = []
 bot.dps_reactions = []
 bot.keystone_reactor = None
+start_tank = []
+start_healer = []
+start_dps = []
 
 @bot.event
 async def on_ready():
@@ -35,33 +37,37 @@ async def on_reaction_add(reaction, user):
     
     message = reaction.message
     if '<@&724869170734432258>' in message.content:
-        print(user.name)
         if str(reaction.emoji) == '<:Tank:1095150384164634624>':
-            print(f"{bot.tank_reactions}")
             bot.tank_reactions.append(user)
-            print(f"Added {user.name} {len(bot.tank_reactions)}to tank reactions list")
-            print(f"tankowie", bot.tank_reactions)
         elif str(reaction.emoji) == '<:Healer:1095151227379130418>':
             bot.healer_reactions.append(user)
-            print(f"Added {user.name} {len(bot.healer_reactions)} to healer reactions list")
         elif str(reaction.emoji) == '<:DPS:1095151144864579725>':
             bot.dps_reactions.append(user)
-            print(f"Added {user.name} {len(bot.dps_reactions)}to DPS reactions list")
+        if user in bot.tank_reactions and user not in bot.healer_reactions and user not in bot.dps_reactions:
+            start_tank.append(user)
+        if user in bot.healer_reactions and user not in bot.tank_reactions and user not in bot.dps_reactions:
+            start_healer.append(user)
+        if user in bot.dps_reactions and user not in bot.tank_reactions and user not in bot.healer_reactions:
+            start_dps.append(user)
 
-        if len(bot.tank_reactions) >= 1 and len(bot.healer_reactions) >= 1 and len(bot.dps_reactions) >= 2:
+        if len(start_tank) >= 1:
+            if len(start_healer) >= 1:
+                if len(start_dps) >= 2:
+                    await message.channel.send(f"Group can now be made")
+                    time.sleep(3)
+                    
+                    tank_users = random.choice([u for u in bot.tank_reactions])
+                    healer_users = random.choice([u for u in bot.healer_reactions if user not in tank_users])
+                    dps_users = random.sample([u for u in bot.dps_reactions if user not in tank_users and user not in healer_users], 2)
+
+                    await message.channel.send(f"Keystone team: \n<:Tank:1095150384164634624> {tank_users[0].mention} \n<:Healer:1095151227379130418> {healer_users[0].mention} \n<:DPS:1095151144864579725> {dps_users[0].mention} \n<:DPS:1095151144864579725> {dps_users[1].mention}")
+                    # await message.channel.send(f"Keystone team: \n<:Tank:1095150384164634624> {''.join([user.mention for user in tank_users])} \n<:Healer:1095151227379130418> {''.join([user.mention for user in healer_users])} \n<:DPS:1095151144864579725> {''.join([user.mention for user in dps_users])} \n<:DPS:1095151144864579725> {''.join([user.mention for user in dps_users2])}")
+            # else:
+    #     await message.channel.send("Not enough players to form a team yet.")
         
-            tank_users = [random.choice([user for user in bot.tank_reactions])] 
-            healer_users = ([random.choice([user for user in bot.healer_reactions if user not in tank_users])])  # select one healer user
-            dps_users = ([random.choice([user for user in bot.dps_reactions if user not in tank_users and user not in healer_users])])  # select two dps users
-            dps_users2 = ([random.choice([user for user in bot.dps_reactions if user not in tank_users and user not in healer_users and user not in dps_users])])
-            print(f"dupa123")
+                    bot.tank_reactions.clear()
+                    bot.healer_reactions.clear()
+                    bot.dps_reactions.clear()
 
-            await message.channel.send(f"Keystone team: \n<:Tank:1095150384164634624> {''.join([user.mention for user in tank_users])} \n<:Healer:1095151227379130418> {''.join([user.mention for user in healer_users])} \n<:DPS:1095151144864579725> {''.join([user.mention for user in dps_users])} \n<:DPS:1095151144864579725> {''.join([user.mention for user in dps_users2])}")
-        # else:
-        #     await message.channel.send("Not enough players to form a team yet.")
-            
-            bot.tank_reactions.clear()
-            bot.healer_reactions.clear()
-            bot.dps_reactions.clear()
 
-bot.run()
+bot.run(TOKEN)
